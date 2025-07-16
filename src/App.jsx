5e8ba111ece4b57cd5aa403fe32cad97cb18b035
yash1raj234 <yashrajrai1234@gmail.com>
2025-07-17 00:29:17 +0530

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "./sections/navbar";
+import Navbar from "./sections/Navbar";
 import Hero from "./sections/Hero";
 import About from "./sections/About";
 import Projects from "./sections/Projects";
